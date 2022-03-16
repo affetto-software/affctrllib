f5b36b77387efdae3aaafea0b3eb7b036ef069d2
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2022-03-16 18:01:19 +0900

    feat: Add AffettoMock class

---
+++ affctrllib/affmock.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+from typing import Any
+
+import tomli
+
+
+class Node(object):
+    _ip: str
+    _port: int
+
+    @property
+    def ip(self) -> str:
+        return self._ip
+
+    @ip.setter
+    def ip(self, ip: str) -> None:
+        self._ip = ip
+
+    @property
+    def port(self) -> int:
+        return self._port
+
+    @port.setter
+    def port(self, port) -> None:
+        self._port = port
+
+
+class AffettoMock(object):
+    config_path: Path | None
+    config_dict: dict[str, Any]
+    local_node: Node
+    remote_node: Node
+
+    def __init__(self) -> None:
+        self.config_path = None
+        self.config_dict = {}
+
+    def __repr__(self) -> str:
+        return "affctrllib.affmock.AffettoMock()"
+
+    def load_config(self, config_path: str | Path) -> None:
+        self.config_path = Path(config_path)
+        with open(self.config_path, "rb") as f:
+            self.config_dict = tomli.load(f)
+
+        self.local_node = Node()
+        self.local_node.ip = "localhost"
+        self.local_node.port = 50010
+        self.remote_node = Node()
+        self.remote_node.ip = "localhost"
+        self.remote_node.port = 50000
---
+++ tests/test_affmock.py
@@ -0,0 +1,29 @@
+import os
+
+from affctrllib.affmock import AffettoMock
+
+config_dir_path = os.path.join(os.path.dirname(__file__), "config")
+
+
+class TestAffComm:
+    def test_init(self) -> None:
+        mock = AffettoMock()
+        assert mock.config_path == None
+        assert mock.config_dict == {}
+
+    def test_repr(self) -> None:
+        mock = AffettoMock()
+        assert repr(mock) == "affctrllib.affmock.AffettoMock()"
+
+    def test_load_config(self) -> None:
+        mock = AffettoMock()
+        mock.load_config(os.path.join(config_dir_path, "mock.toml"))
+        assert isinstance(mock.config_dict, dict)
+
+    def test_load_config_remote(self) -> None:
+        mock = AffettoMock()
+        mock.load_config(os.path.join(config_dir_path, "mock.toml"))
+        assert mock.local_node.ip == "localhost"
+        assert mock.local_node.port == 50010
+        assert mock.remote_node.ip == "localhost"
+        assert mock.remote_node.port == 50000
