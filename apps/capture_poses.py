629afaeee68a1c9dd8df0ef26df7ff42806992ac
Hiroshi Atsuta <atsuta@ieee.org>
2022-04-15 22:13:08 +0900

    fix: Handle unprocessed data while waiting

--- apps/capture_poses.py
+++ apps/capture_poses.py
@@ -2,6 +2,8 @@
 
 import argparse
 import os
+import sys
+import threading
 from pathlib import Path
 
 import affctrllib as acl
@@ -13,13 +15,16 @@ BUFSIZE = 1024
 
 
 def usage():
-    print(f"Operations: <Key>")
-    print(f"   capture: <RET>")
-    print(f"   quit:    <q>")
+    print(f" ======= Operations ======= : <Key>")
+    print(f"      preview current angles: <RET>")
+    print(f"      capture current angles: <c>")
+    print(f"save captured angles to file: <s>")
+    print(f"               quit and save: <q>")
+    print(f"         quit without saving: <q!>")
     print(f"")
 
 
-def make_export_string(frames):
+def make_toml_string(frames):
     dof = 13
     profile = "triangular"
 
@@ -41,15 +46,37 @@ def make_export_string(frames):
     return string
 
 
-def export(frames, output):
-    string = make_export_string(frames)
+def save(frames, output):
+    string = make_toml_string(frames)
     if output is None:
         print(string)
     else:
-        path = Path(output)
-        with open(path.with_suffix(".toml"), mode="w") as fobj:
+        path = Path(output).with_suffix(".toml")
+        with open(path, mode="w") as fobj:
             fobj.write(string)
         print(f"Saved captured keyframes in <{str(path)}>")
+
+
+class ReceivedBytes(object):
+    _bytes: bytes
+
+    def __init__(self):
+        self._bytes = bytes()
+        self._lock = threading.Lock()
+
+    def get(self) -> bytes:
+        with self._lock:
+            return self._bytes
+
+    def set(self, _bytes: bytes) -> None:
+        with self._lock:
+            self._bytes = _bytes
+
+
+def receive_continuously(ssock, recv_bytes: ReceivedBytes, event: threading.Event):
+    while event.is_set():
+        rb, _ = ssock.recvfrom(BUFSIZE)
+        recv_bytes.set(rb)
 
 
 def mainloop(config, output):
@@ -57,26 +84,50 @@ def mainloop(config, output):
     print(acom)
     usage()
 
-    timer = Timer()
-    frames = []
+    # Create a thread to receive continuously.
     ssock = acom.create_sensory_socket()
+    recv_bytes = ReceivedBytes()
+    event = threading.Event()
+    th = threading.Thread(target=receive_continuously, args=(ssock, recv_bytes, event))
+    event.set()
+    th.start()
 
+    def cleanup():
+        event.clear()
+        th.join()
+        ssock.close()
+
+    frames = []
+    timer = Timer()
     t = 0
     timer.start()
-    while True:
-        c = input("> ")
-        if c == "q":
-            print("Quitting...")
-            break
-        t = timer.elapsed_time()
-        recv_bytes, _ = ssock.recvfrom(BUFSIZE)
-        sarr = acl.split_received_msg(recv_bytes, function=int)
-        q = acl.unzip_array(sarr)[0]
-        frames.append((t, q))
-        print("Successfully captured!")
-
-    ssock.close()
-    export(frames, output)
+    try:
+        while True:
+            c = input("> ")
+            t = timer.elapsed_time()
+            sarr = acl.split_received_msg(recv_bytes.get())
+            q = acl.unzip_array(sarr)[0]
+            if c == "q":
+                print("Quitting...")
+                save(frames, output)
+                break
+            elif c == "q!":
+                print("Quitting without saving...")
+                break
+            elif c == "c":
+                frames.append((t, q))
+                print(f"q={q}")
+                print("Successfully captured!")
+            elif c == "s":
+                save(frames, output)
+                print("Successfully saved!")
+            else:
+                print(f"q={q}")
+    except KeyboardInterrupt:
+        print("Caught KeyboardInterrupt")
+        cleanup()
+        sys.exit(1)
+    cleanup()
 
 
 def parse():
