7d737cd5292193f8fa9e02f8a7fe9f7ef02b1ad4
Hiroshi Atsuta <atsuta@ieee.org>
2022-04-18 21:20:54 +0900

    feat: Add Socket class

--- affctrllib/_sockutil.py
+++ affctrllib/_sockutil.py
@@ -1,5 +1,4 @@
-from __future__ import annotations
-
+import socket as sock
 from typing import Any
 
 
@@ -54,3 +53,117 @@ class SockAddr(object):
         if self.port is None:
             raise RuntimeError(f"SockAddr: no port is provided for '{self.host}'")
         return (self.host, self.port)
+
+
+class Socket(object):
+
+    _family: str
+    _host: str
+    _port: int
+    _socket: sock.socket
+
+    def __init__(
+        self,
+        addr: tuple[str, int] | dict[str, str | int] | None = None,
+        host: str | None = None,
+        port: int | None = None,
+    ) -> None:
+        self._family = "AF_INET"
+        if addr is not None:
+            self.addr = addr
+        if host is not None:
+            self.host = host
+        if port is not None:
+            self.port = port
+
+    def __repr__(self) -> str:
+        try:
+            return f"Socket(({self.host}, {str(self.port)}))"
+        except AttributeError:
+            return f"Socket()"
+
+    def __str__(self) -> str:
+        try:
+            socket_type = self._socket.type
+        except AttributeError:
+            socket_type = sock.SOCK_DGRAM
+        socket_type_name = ""
+        if socket_type == sock.SOCK_DGRAM:
+            socket_type_name = "UDP"
+        elif socket_type == sock.SOCK_STREAM:
+            socket_type_name = "TCP"
+        else:
+            socket_type_name = str(socket_type).split(".")[1]
+        try:
+            return f"{socket_type_name}: {self.host}:{str(self.port)}"
+        except AttributeError:
+            return f"{socket_type_name}: No address is provided"
+
+    @property
+    def family(self) -> str:
+        return self._family
+
+    @property
+    def host(self) -> str:
+        return self._host
+
+    @host.setter
+    def host(self, host: str) -> None:
+        self._host = host
+
+    @property
+    def port(self) -> int:
+        return self._port
+
+    @port.setter
+    def port(self, port: int) -> None:
+        self._port = port
+
+    @property
+    def addr(self) -> tuple[str, int]:
+        return (self.host, self.port)
+
+    @addr.setter
+    def addr(self, addr: tuple[str, int] | dict[str, int | str]) -> None:
+        if isinstance(addr, tuple):
+            self.host = addr[0]
+            self.port = addr[1]
+        elif isinstance(addr, dict):
+            self.host = str(addr["host"])
+            self.port = int(addr["port"])
+        else:
+            raise TypeError(f"unsupported type for addr: {type(addr)}")
+
+    @property
+    def socket(self) -> sock.socket:
+        try:
+            return self._socket
+        except AttributeError:
+            raise RuntimeError("No socket is created yet")
+
+    def create(self, socket_type=sock.SOCK_DGRAM) -> sock.socket:
+        self._socket = sock.socket(getattr(sock, self.family), socket_type)
+        return self._socket
+
+    def bind(self, addr: tuple[str, int] | None = None) -> None:
+        if addr is None:
+            try:
+                addr = self.addr
+            except AttributeError:
+                raise RuntimeError("No address is provided to bind socket")
+        self.socket.bind(addr)
+
+    def recvfrom(self, bufsize=1024) -> bytes:
+        recv_bytes, _ = self.socket.recvfrom(bufsize)
+        return recv_bytes
+
+    def sendto(self, send_bytes: bytes, addr: tuple[str, int] | None = None) -> int:
+        if addr is None:
+            try:
+                addr = self.addr
+            except AttributeError:
+                raise RuntimeError("No address is provided to send to")
+        return self.socket.sendto(send_bytes, addr)
+
+    def close(self) -> None:
+        self.socket.close()
--- tests/test_sockutil.py
+++ tests/test_sockutil.py
@@ -1,5 +1,7 @@
+import socket
+
 import pytest
-from affctrllib._sockutil import SockAddr
+from affctrllib._sockutil import SockAddr, Socket
 
 
 class TestSockAddr:
@@ -153,3 +155,164 @@ class TestSockAddr:
         with pytest.raises(RuntimeError) as excinfo:
             _ = sa.addr
         assert f"SockAddr: no port is provided for '{host}'" in str(excinfo.value)
+
+
+class TestSocket:
+    def test_init(self) -> None:
+        s = Socket()
+        assert s.family == "AF_INET"
+
+    def test_init_with_addr(self) -> None:
+        addr = ("192.168.10.10", 10)
+        s = Socket(addr)
+        assert s.family == "AF_INET"
+        assert s.host == addr[0]
+        assert s.port == addr[1]
+        assert s.addr == addr
+
+    @pytest.mark.parametrize("host", ["localhost", "192.168.11.1", "192.168.11.3"])
+    def test_init_host(self, host: str) -> None:
+        s = Socket(host=host)
+        assert s.host == host
+
+    @pytest.mark.parametrize("port", [0, 80, 1111])
+    def test_init_port(self, port: int) -> None:
+        s = Socket(port=port)
+        assert s.port == port
+
+    @pytest.mark.parametrize(
+        "addr",
+        [
+            ("localhost", 80),
+            ("192.168.1.1", 1000),
+            ("192.168.5.10", 3000),
+        ],
+    )
+    def test_init_addr(self, addr: tuple[str, int]) -> None:
+        s = Socket(addr=addr)
+        assert s.host == addr[0]
+        assert s.port == addr[1]
+        assert s.addr == addr
+
+    @pytest.mark.parametrize(
+        "host,port",
+        [
+            ("localhost", 1000),
+            ("192.168.1.1", 2000),
+            ("192.168.12.34", 3000),
+        ],
+    )
+    def test_repr(self, host: str, port: int) -> None:
+        s = Socket((host, port))
+        assert repr(s) == f"Socket(({host}, {str(port)}))"
+
+    def test_repr_no_address(self) -> None:
+        s = Socket()
+        assert repr(s) == f"Socket()"
+
+    @pytest.mark.parametrize(
+        "host,port",
+        [
+            ("localhost", 1010),
+            # ("192.168.2.22", 2020),
+            # ("192.168.3.33", 3030),
+        ],
+    )
+    def test_str(self, host: str, port: int) -> None:
+        s = Socket((host, port))
+        assert str(s) == f"UDP: {host}:{str(port)}"
+
+    def test_str_no_address(self) -> None:
+        s = Socket()
+        assert str(s) == f"UDP: No address is provided"
+
+    @pytest.mark.parametrize("host", ["localhost", "192.168.11.4", "192.168.11.5"])
+    def test_host_setter(self, host: str) -> None:
+        s = Socket()
+        s.host = host
+        assert s.host == host
+
+    @pytest.mark.parametrize("port", [10, 20, 30])
+    def test_port_setter(self, port: int) -> None:
+        s = Socket()
+        s.port = port
+        assert s.port == port
+
+    @pytest.mark.parametrize(
+        "addr",
+        [
+            ("localhost", 8080),
+            ("192.168.5.1", 1010),
+            ("192.168.5.2", 3030),
+        ],
+    )
+    def test_addr_setter(self, addr: tuple[str, int]) -> None:
+        s = Socket()
+        s.addr = addr
+        assert s.host == addr[0]
+        assert s.port == addr[1]
+        assert s.addr == addr
+
+    @pytest.mark.parametrize(
+        "addr_dict",
+        [
+            {"host": "localhost", "port": 800},
+            {"host": "192.168.1.1", "port": 700},
+            {"host": "192.168.1.3", "port": 500},
+        ],
+    )
+    def test_set_addr_dict(self, addr_dict: dict[str, int | str]) -> None:
+        s = Socket()
+        s.addr = addr_dict
+        assert s.host == addr_dict["host"]
+        assert s.port == addr_dict["port"]
+
+    def test_create(self) -> None:
+        s = Socket(("localhost", 10000))
+        s.create()
+        assert s.socket.family == socket.AF_INET
+        assert s.socket.type == socket.SOCK_DGRAM
+
+    def test_socket_error(self) -> None:
+        s = Socket()
+        with pytest.raises(RuntimeError) as excinfo:
+            _ = s.socket
+        assert str(excinfo.value) == "No socket is created yet"
+
+    def test_bind_error_no_address(self) -> None:
+        s = Socket()
+        s.create()
+        with pytest.raises(RuntimeError) as excinfo:
+            s.bind()
+        assert str(excinfo.value) == "No address is provided to bind socket"
+
+    def test_bind_error_not_created(self) -> None:
+        s = Socket(("localhost", 10000))
+        with pytest.raises(RuntimeError) as excinfo:
+            s.bind()
+        assert str(excinfo.value) == "No socket is created yet"
+
+    def test_recvfrom_error_not_created(self) -> None:
+        s = Socket()
+        with pytest.raises(RuntimeError) as excinfo:
+            _ = s.recvfrom()
+        assert str(excinfo.value) == "No socket is created yet"
+
+    def test_sendto_error_no_address(self) -> None:
+        s = Socket()
+        s.create()
+        with pytest.raises(RuntimeError) as excinfo:
+            s.sendto(b"")
+        assert str(excinfo.value) == "No address is provided to send to"
+
+    def test_sendto_error_not_created(self) -> None:
+        s = Socket(("192.168.1.1", 10000))
+        with pytest.raises(RuntimeError) as excinfo:
+            s.sendto(b"")
+        assert str(excinfo.value) == "No socket is created yet"
+
+    def test_close_error_not_created(self) -> None:
+        s = Socket()
+        with pytest.raises(RuntimeError) as excinfo:
+            s.close()
+        assert str(excinfo.value) == "No socket is created yet"
