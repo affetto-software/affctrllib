76560ed476c255bc4014def42b82802ae0624d54
Hiroshi Atsuta <atsuta@ieee.org>
2022-05-02 19:38:55 +0900

    fix: Trapez velocity profile is used

--- apps/ptp_test.py
+++ apps/ptp_test.py
@@ -5,12 +5,18 @@ from affctrllib import PTP, Logger
 
 q0 = np.array([0, 0, 127])
 qF = np.array([127, 255, 0])
+vmax = np.array([16, 32, 16])
+tb = np.array([2, 3, 4])
 T = 10
 t0 = 0
-profile = "tri"  # or "5th"
+profile = "trapez"  # or "5th" or "tri"
 N = 1000
 
-ptp = PTP(q0, qF, T, t0, profile_name=profile)
+if profile in ["tri", "5th"]:
+    ptp = PTP(q0, qF, T, t0, profile_name=profile)
+else:
+    ptp = PTP(q0, qF, T, t0, vmax=vmax, profile_name=profile)
+    # ptp = PTP(q0, qF, T, t0, tb=tb, profile_name=profile)
 logger = Logger()
 logger.set_labels(["t", "q0", "q1", "q2", "dq0", "dq1", "dq2", "ddq0", "ddq1", "ddq2"])
 dt = (t0 + T) / N
@@ -19,7 +25,7 @@ for i in range(N):
     t = i * dt
     q = ptp.q(t)
     dq = ptp.dq(t)
-    ddq = (ptp.dq(t) - dq_prev) / dt  # type: ignore
+    ddq = (ptp.dq(t) - dq_prev) / dt
     dq_prev = dq
-    logger.store_data(np.concatenate(([t], q, dq, ddq)))  # type: ignore
+    logger.store_data(np.concatenate(([t], q, dq, ddq)))
 logger.dump()
