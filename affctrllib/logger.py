b7a6c9b751f91f4b89f52bcdb8194d8deee5136c
Hiroshi Atsuta <atsuta@ieee.org>
2022-05-19 17:14:23 +0900

    fix: Make Logger class thread safe

--- affctrllib/logger.py
+++ affctrllib/logger.py
@@ -1,6 +1,8 @@
+import copy
 import sys
 from collections.abc import Iterable
 from pathlib import Path
+from threading import Lock
 from typing import Any
 
 
@@ -10,15 +12,17 @@ class Logger(object):
     _labels: list[str]
     _rawdata: list[list[Any]]
     _fpath: Path
+    _lock: Lock
 
     def __init__(
         self, fname: str | Path | None = None, sep: str = ",", eol: str = "\n"
     ) -> None:
-        self.fpath = fname
         self._sep = sep
         self._eol = eol
         self._labels = []
         self._rawdata = []
+        self._lock = Lock()
+        self.fpath = fname
 
     @property
     def sep(self) -> str:
@@ -31,17 +35,20 @@ class Logger(object):
     @property
     def fpath(self) -> str | None:
         try:
-            return str(self._fpath)
+            with self._lock:
+                return str(self._fpath)
         except AttributeError:
             return None
 
     @fpath.setter
     def fpath(self, fname: str | Path | None) -> None:
         if fname is not None:
-            self._fpath = Path(fname)
+            with self._lock:
+                self._fpath = Path(fname)
         else:
             try:
-                del self._fpath
+                with self._lock:
+                    del self._fpath
             except AttributeError:
                 pass
 
@@ -53,21 +60,26 @@ class Logger(object):
 
     def set_labels(self, *args: str | Iterable[str]) -> None:
         listed = [[arg] if isinstance(arg, str) else arg for arg in args]
-        self._labels = [x for l in listed for x in l]
+        with self._lock:
+            self._labels = [x for l in listed for x in l]
 
     def extend_labels(self, *args: str | Iterable[str]) -> None:
         listed = [[arg] if isinstance(arg, str) else arg for arg in args]
-        self._labels.extend([x for l in listed for x in l])
+        with self._lock:
+            self._labels.extend([x for l in listed for x in l])
 
     def get_header(self) -> str:
-        return self.sep.join(self._labels)
+        with self._lock:
+            return self.sep.join(self._labels)
 
     def store_data(self, data: Iterable[Any]) -> None:
-        self._rawdata.append(list(data))
+        with self._lock:
+            self._rawdata.append(list(data))
 
     def extend_data(self, data: Iterable[Any]) -> None:
         try:
-            self._rawdata[-1].extend(list(data))
+            with self._lock:
+                self._rawdata[-1].extend(list(data))
         except IndexError:
             self.store_data(data)
 
@@ -77,7 +89,12 @@ class Logger(object):
         self.store_data(data)
 
     def get_data(self) -> list[list[Any]]:
-        return self._rawdata
+        with self._lock:
+            return self._rawdata
+
+    def copy_data(self) -> list[list[Any]]:
+        with self._lock:
+            return copy.deepcopy(self._rawdata)
 
     def _slice_data_get_index_list(
         self, label: str | int | Iterable[str | int]
@@ -98,14 +115,15 @@ class Logger(object):
         label: str | int | Iterable[str | int],
         index: tuple[int, int] | None = None,
     ) -> list[list[Any]]:
-        cols = self._slice_data_get_index_list(label)
-        row_range = (0, len(self._rawdata))
-        if index is not None:
-            row_range = (
-                index[0] if len(index) > 0 else 0,
-                index[1] if len(index) > 1 else len(self._rawdata),
-            )
-        extracted_data = self._rawdata[row_range[0] : row_range[1]]
+        with self._lock:
+            cols = self._slice_data_get_index_list(label)
+            row_range = (0, len(self._rawdata))
+            if index is not None:
+                row_range = (
+                    index[0] if len(index) > 0 else 0,
+                    index[1] if len(index) > 1 else len(self._rawdata),
+                )
+            extracted_data = self._rawdata[row_range[0] : row_range[1]]
         return [[row[c] for row in extracted_data] for c in cols]
 
     def _generate_alternative_fname(self, basefn: Path) -> Path:
@@ -121,16 +139,19 @@ class Logger(object):
     def _dump_print(self, output=None) -> None:
         if len(self._labels):
             print(self.get_header(), end=self.eol, file=output)
-        for line in self._rawdata:
-            print(self.sep.join(f"{x}" for x in line), end=self.eol, file=output)
+        with self._lock:
+            for line in self._rawdata:
+                print(self.sep.join(f"{x}" for x in line), end=self.eol, file=output)
 
     def _dump_open(self, fpath: Path, mode: str) -> None:
         with open(fpath, mode=mode) as fobj:
             if len(self._labels):
                 fobj.write(self.get_header() + self.eol)
-            fobj.writelines(
-                self.sep.join(f"{x}" for x in line) + self.eol for line in self._rawdata
-            )
+            with self._lock:
+                fobj.writelines(
+                    self.sep.join(f"{x}" for x in line) + self.eol
+                    for line in self._rawdata
+                )
 
     def dump(
         self,
