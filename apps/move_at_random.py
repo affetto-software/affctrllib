7675a5197705d28d74d596c3090e1172f3ad2d7b
Hiroshi Atsuta <atsuta@ieee.org>
2023-06-04 13:32:07 +0900

    Fix default parameter

--- apps/move_at_random.py
+++ apps/move_at_random.py
@@ -11,7 +11,7 @@ from affctrllib import PTP, AffPosCtrlThread, Logger
 DEFAULT_CONFIG_PATH = Path(__file__).parent.joinpath("config.toml")
 
 DEFAULT_UPDATE_T_RANGE = (0.5, 1.5)
-DEFAULT_UPDATE_Q_RANGE = (15.0, 40.0)
+DEFAULT_UPDATE_Q_RANGE = (20.0, 40.0)
 DEFAULT_Q_LIMIT = (5.0, 95.0)
 
 # Set printing options for numpy array.
