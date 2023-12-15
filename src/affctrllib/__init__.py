be640871cfece67476f0c83506f057a24281135d
Hiroshi Atsuta <atsuta@ieee.org>
2023-12-15 19:04:04 +0900

    fix: Fix `__all__` variable defined in `__init__.py`

--- src/affctrllib/__init__.py
+++ src/affctrllib/__init__.py
@@ -1,9 +1,16 @@
+"""Affetto Control Library.
+
+This is a small library to control NEDO Affetto in Python.
+"""
+
+from __future__ import annotations
+
 from importlib import metadata
 
 __version__ = metadata.version("affctrllib")
 __all__ = ["__version__"]
 
-from .affcomm import (
+from affctrllib.affcomm import (
     AffComm,
     convert_array_to_bytes,
     convert_array_to_string,
@@ -13,48 +20,35 @@ from .affcomm import (
     zip_arrays,
     zip_arrays_as_ndarray,
 )
+from affctrllib.affctrl import AffCtrl, AffCtrlThread
+from affctrllib.affmock import AffMock
+from affctrllib.affposctrl import AffPosCtrl, AffPosCtrlThread
+from affctrllib.affstate import AffState, AffStateThread
+from affctrllib.filter import Filter
+from affctrllib.logger import Logger
+from affctrllib.ptp import PTP
+from affctrllib.timer import Timer
 
-__all__.extend(
-    [
-        "AffComm",
-        "convert_array_to_bytes",
-        "convert_array_to_string",
-        "unzip_array_as_ndarray",
-        "split_received_msg",
-        "unzip_array",
-        "zip_arrays",
-        "zip_arrays_as_ndarray",
-    ]
-)
-
-from .affctrl import AffCtrl, AffCtrlThread
-
-__all__.extend(["AffCtrl", "AffCtrlThread"])
-
-from .affmock import AffMock
-
-__all__.extend(["AffMock"])
-
-from .affposctrl import AffPosCtrl, AffPosCtrlThread
-
-__all__.extend(["AffPosCtrl", "AffPosCtrlThread"])
-
-from .affstate import AffState, AffStateThread
-
-__all__.extend(["AffState", "AffStateThread"])
-
-from .filter import Filter
-
-__all__.extend(["Filter"])
-
-from .logger import Logger
-
-__all__.extend(["Logger"])
-
-from .ptp import PTP
-
-__all__.extend(["PTP"])
-
-from .timer import Timer
-
-__all__.extend(["Timer"])
+__all__ = [
+    "AffComm",
+    "AffCtrl",
+    "AffCtrl",
+    "AffCtrlThread",
+    "AffCtrlThread",
+    "AffMock",
+    "AffPosCtrl",
+    "AffPosCtrlThread",
+    "AffState",
+    "AffStateThread",
+    "Filter",
+    "Logger",
+    "PTP",
+    "Timer",
+    "convert_array_to_bytes",
+    "convert_array_to_string",
+    "split_received_msg",
+    "unzip_array",
+    "unzip_array_as_ndarray",
+    "zip_arrays",
+    "zip_arrays_as_ndarray",
+]
