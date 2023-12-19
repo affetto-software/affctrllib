d42fc37d13f50c8490f119943212aeac3f0ea5ad
Hiroshi Atsuta <atsuta@ieee.org>
2023-12-19 18:30:16 +0900

    test: Fix testing configuration for nox

--- noxfile.py
+++ noxfile.py
@@ -7,5 +7,5 @@ os.environ.update(PDM_IGNORE_SAVED_PYTHON="1", PDM_USE_VENV="1")
 
 @nox.session(python=("3.10", "3.11", "3.12"))
 def tests(session):
-    session.run("pdm", "install", "-Gtests", external=True)
+    session.run_always("pdm", "install", "-G", "tests", external=True)
     session.run("pytest", "tests/")
