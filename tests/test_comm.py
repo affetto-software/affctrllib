1d20d9b3f847b421fe124481683c7adf290fa05a
Hiroshi Atsuta <atsuta@ieee.org>
2024-01-19 11:56:16 +0900

    fix: Change definition order of functions and classes

--- tests/test_comm.py
+++ tests/test_comm.py
@@ -30,65 +30,6 @@ def default_socket() -> IPv4Socket:
     return IPv4Socket(default_address())
 
 
-def test_check_repr(default_socket: IPv4Socket) -> None:
-    assert repr(default_socket) == "IPv4Socket(('localhost', 123456), SOCK_DGRAM, nonblock=False)"
-
-
-def test_check_str(default_socket: IPv4Socket) -> None:
-    assert str(default_socket) == "IPv4Socket<localhost:123456> (UDP)"
-    s = IPv4Socket(default_address(), SOCK_STREAM)
-    assert str(s) == "IPv4Socket<localhost:123456> (TCP)"
-    s = IPv4Socket(default_address(), nonblock=True)
-    assert str(s) == "IPv4Socket<localhost:123456> (UDP,nonblock)"
-
-
-@pytest.mark.parametrize("host,port", [("localhost", 123456), ("192.168.5.11", 222222)])
-def test_check_socket_attributes(host: str, port: int) -> None:
-    """Check if a IPv4 socket object is created based on the given address.
-
-    Parameters
-    ----------
-    host : str
-        The host address.
-    port : int
-        The port number.
-    """
-
-    s = IPv4Socket((host, port))
-    assert type(s.socket) == socket.socket
-    assert s.address == (host, port)
-    assert s.host == host
-    assert s.port == port
-    assert s.family == AF_INET
-    assert s.type == SOCK_DGRAM
-    assert s.proto == 0
-
-
-@pytest.mark.parametrize("socket_type", [SOCK_DGRAM, SOCK_STREAM])
-def test_specify_socket_type(socket_type: int) -> None:
-    """Check if a created socket has the given socket type.
-
-    Parameters
-    ----------
-    socket_type : int
-        The socket type.
-    """
-    s = IPv4Socket(("localhost", 123456), socket_type=socket_type)
-    assert s.type == socket_type
-
-
-def test_enable_blocking_mode_by_default(default_socket: IPv4Socket) -> None:
-    """Test that the blocking mode is enabled by default."""
-    assert default_socket.is_nonblocking() == False
-
-
-@pytest.mark.parametrize("nonblock", [False, True])
-def test_check_nonblocking_mode(nonblock: bool) -> None:
-    """Check if `is_nonblocking` returns False in blocking mode."""
-    s = IPv4Socket(("localhost", 123456), nonblock=nonblock)
-    assert s.is_nonblocking() == nonblock
-
-
 @pytest.mark.parametrize(
     "data,expected",
     [("1 2 3", [1, 2, 3]), ("11 22 33 44", [11, 22, 33, 44]), (" 1  2    3  ", [1, 2, 3])],
@@ -362,3 +303,62 @@ def test_zip_items_fails_if_sizes_of_lines_not_match() -> None:
     with pytest.raises(ValueError) as e:
         zip_items([1, 2, 3], [4, 5, 6, 7])
     assert "all input items must have the same size" in str(e.value)
+
+
+def test_check_repr(default_socket: IPv4Socket) -> None:
+    assert repr(default_socket) == "IPv4Socket(('localhost', 123456), SOCK_DGRAM, nonblock=False)"
+
+
+def test_check_str(default_socket: IPv4Socket) -> None:
+    assert str(default_socket) == "IPv4Socket<localhost:123456> (UDP)"
+    s = IPv4Socket(default_address(), SOCK_STREAM)
+    assert str(s) == "IPv4Socket<localhost:123456> (TCP)"
+    s = IPv4Socket(default_address(), nonblock=True)
+    assert str(s) == "IPv4Socket<localhost:123456> (UDP,nonblock)"
+
+
+@pytest.mark.parametrize("host,port", [("localhost", 123456), ("192.168.5.11", 222222)])
+def test_check_socket_attributes(host: str, port: int) -> None:
+    """Check if a IPv4 socket object is created based on the given address.
+
+    Parameters
+    ----------
+    host : str
+        The host address.
+    port : int
+        The port number.
+    """
+
+    s = IPv4Socket((host, port))
+    assert type(s.socket) == socket.socket
+    assert s.address == (host, port)
+    assert s.host == host
+    assert s.port == port
+    assert s.family == AF_INET
+    assert s.type == SOCK_DGRAM
+    assert s.proto == 0
+
+
+@pytest.mark.parametrize("socket_type", [SOCK_DGRAM, SOCK_STREAM])
+def test_specify_socket_type(socket_type: int) -> None:
+    """Check if a created socket has the given socket type.
+
+    Parameters
+    ----------
+    socket_type : int
+        The socket type.
+    """
+    s = IPv4Socket(("localhost", 123456), socket_type=socket_type)
+    assert s.type == socket_type
+
+
+def test_enable_blocking_mode_by_default(default_socket: IPv4Socket) -> None:
+    """Test that the blocking mode is enabled by default."""
+    assert default_socket.is_nonblocking() == False
+
+
+@pytest.mark.parametrize("nonblock", [False, True])
+def test_check_nonblocking_mode(nonblock: bool) -> None:
+    """Check if `is_nonblocking` returns False in blocking mode."""
+    s = IPv4Socket(("localhost", 123456), nonblock=nonblock)
+    assert s.is_nonblocking() == nonblock
