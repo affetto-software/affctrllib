a88320e846a0ba54301edd79ef570dd728bc9d76
Hiroshi Atsuta <atsuta@ieee.org>
2024-03-15 19:13:43 +0900

    wip

---
+++ src/affctrllib/sensor.py
@@ -0,0 +1,6 @@
+"""This module processes data sent from the sensory unit.
+
+It is designed to process data that received by `comm` module and convert it so that it can be interpretted by human, i.e. it is converted into quantative units.
+"""
+
+from __future__ import annotations
---
+++ tests/test_sensor.py
@@ -0,0 +1,7 @@
+"""Tests for `sensor` module."""
+
+from __future__ import annotations
+
+
+def test_fail():
+    assert False
