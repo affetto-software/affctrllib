c5570c6a0a66c7553adbe95192d899bf9e9b5736
Hiroshi Atsuta <atsuta@ieee.org>
2024-03-28 15:40:00 +0900

    feat: Add fundamental functions to rate module
    Functions that handle cycling loops in a fixed frequency are added to
    the `rate` module.

--- src/affctrllib/rate.py
+++ src/affctrllib/rate.py
@@ -2,11 +2,32 @@
 
 from __future__ import annotations
 
+import time
+from functools import cache, cached_property
+
 from affctrllib.const import TOL
+
+
+def timestamp() -> float:
+    """Return the time in seconds since the epoch.
+
+    Returns
+    -------
+    float
+        The time in seconds since the epoch.
+    """
+    # return time.monotonic()
+    # return time.perf_counter()
+    # return time.process_time()
+    # return time.thread_time()
+    return time.time()
 
 
 class Rate(object):
     _frequency: float
+    _actual_cycle_time: float
+    _start_timer: float
+    _start: float
 
     def __init__(self, frequency: float | int) -> None:
         """Initialize the Rate object.
@@ -17,6 +38,8 @@ class Rate(object):
             The frequency to be run periodically.
         """
         self._set_frequency(frequency)
+        self._actual_cycle_time = 0.0
+        self.start()
 
     def _set_frequency(self, frequency: float | int) -> float:
         """Set a frequency.
@@ -42,6 +65,131 @@ class Rate(object):
         self._frequency = float(frequency)
         return self._frequency
 
-    @property
+    @cached_property
     def frequency(self) -> float:
+        """Return the frequency.
+
+        Returns
+        -------
+        float
+            The frequency to run a loop in Hertz.
+        """
         return self._frequency
+
+    def cycle_time(self) -> float:
+        """Return the actual execution time in seconds.
+
+        Return the time in seconds that took for execution. Therefore,
+        sleeping time is calculated by substracting this value from
+        the expected cycle time, i.e. the inverse of the given
+        frequency.
+
+        Returns
+        -------
+        float
+            The actual execution time in seconds.
+        """
+        return self._actual_cycle_time
+
+    @cache
+    def expected_cycle_time(self) -> float:
+        """Return the expected cycle time in seconds.
+
+        The value is calculated by inverting the given frequency.
+
+        Returns
+        -------
+        float
+            The expected cycle time for each loop.
+        """
+        return 1.0 / self._frequency
+
+    def now(self) -> float:
+        """Return the time in seconds since the clock started."""
+        return timestamp() - self._start_timer
+
+    def start(self) -> float:
+        """Restart the clock.
+
+        This is useful when resetting immediately before entering main
+        loop after long initialization process.
+
+        Returns
+        -------
+        float
+            The timestamp when the clock started.
+
+        Examples
+        --------
+        >>> r = Rate(10)
+        >>> # Long long initialization process
+        >>> r.start()    # Just before entering loop
+        >>> while True:
+        ...     # Tasks to be executed in each loop
+        ...     # do_some_tasks()
+        ...     r.sleep()
+        """
+        self._start_timer = timestamp()
+        self._start = self._start_timer
+        return self._start_timer
+
+    def reset(self) -> float:
+        """Reset the clock for the current loop.
+
+        This function can be used to reset the actual cycle time for
+        the current loop due to some reason. It doesn't affect the current
+        time elapsed since the clock started.
+
+        Returns
+        -------
+        float
+            The timestamp when the previous loop started.
+
+        Examples
+        --------
+        >>> r = Rate(10)
+        >>> while True:
+        ...     # do_some_tasks()  # Unexpected error occurred
+        ...     r.reset()
+        ...     # do_some_tasks()  # Try again
+        ...     r.sleep()
+        """
+        self._start = timestamp()
+        return self._start
+
+    def sleep(self) -> None:
+        """Sleep the current process (thread) until the expected cycle time is consumed.
+
+        Examples
+        --------
+        >>> r = Rate(10)
+        >>> while True:
+        ...     # Tasks to be executed in each loop
+        ...     # do_some_tasks()
+        ...     r.sleep()
+        """
+        expected_end = self._start + self.expected_cycle_time()
+        actual_end = timestamp()
+
+        # Detect backward jumps in time.
+        if actual_end < self._start:
+            expected_end = actual_end + self.expected_cycle_time()
+
+        # Calculate the time we'll sleep for.
+        sleep_time = expected_end - actual_end
+
+        # Calculate the actual amount of time the loop took.
+        self._actual_cycle_time = actual_end - self._start
+
+        # Reset the start time of the loop.
+        self._start = expected_end
+
+        # If we've taken too much time we won't sleep.
+        if sleep_time <= 0.0:
+            # If we've jumped forward in time, or the loop has taken
+            # more than a full extra cycle, reset our cycle.
+            if actual_end > expected_end + self.expected_cycle_time():
+                self._start = actual_end
+            return None
+
+        return time.sleep(sleep_time)
--- tests/test_rate.py
+++ tests/test_rate.py
@@ -2,17 +2,73 @@
 
 from __future__ import annotations
 
+from time import sleep
+
 import pytest
 
 from affctrllib.rate import Rate
 
+TEST_RATE_TOL = 3.0e-1
+
+
+@pytest.fixture
+def default_rate() -> Rate:
+    return Rate(10)
+
 
 class TestRate:
-    def test_init(self) -> None:
-        rate = Rate(10)
+    def test_init(self, default_rate: Rate) -> None:
+        rate = default_rate
         assert rate.frequency == 10
+        assert rate.cycle_time() == 0
 
     @pytest.mark.parametrize("invalid_freq", [1e-12, 0, -10])
     def test_init_raise_error_when_given_freq_is_zero_or_negative(self, invalid_freq) -> None:
         with pytest.raises(ValueError):
             _ = Rate(invalid_freq)
+
+    @pytest.mark.parametrize("freq,expected", [(1, 1), (10, 0.1), (25, 0.04)])
+    def test_expected_cycle_time(self, freq: float, expected: float) -> None:
+        rate = Rate(freq)
+        assert rate.expected_cycle_time() == expected
+
+    def test_now(self, default_rate: Rate) -> None:
+        rate = default_rate
+        t1 = rate.now()
+        sleep(0.001)
+        t2 = rate.now()
+        assert t1 < t2
+
+    def test_start(self, default_rate: Rate) -> None:
+        rate = default_rate
+        sleep(0.001)
+        t = rate.now()
+        assert t > 0.001
+        # Restart the timer
+        rate.start()
+        sleep(0.001)
+        t = rate.now()
+        assert t == pytest.approx(0.001, rel=TEST_RATE_TOL)
+
+    def test_reset(self) -> None:
+        rate = Rate(100)
+        sleep(0.02)  # took longer than expected
+        rate.reset()  # reset the timer
+        sleep(0.01)  # took within expected time
+        rate.sleep()  # calculate actual cycle time
+        cycle_time = rate.cycle_time()
+        t = rate.now()
+        assert cycle_time == pytest.approx(0.01, rel=TEST_RATE_TOL)
+        # rate.reset does not affect the elapsed time
+        assert t == pytest.approx(0.03, rel=TEST_RATE_TOL)
+
+    def test_sleep(self) -> None:
+        rate = Rate(100)
+        sleep(0.005)
+        rate.sleep()
+        t = rate.now()
+        assert t == pytest.approx(0.01, rel=TEST_RATE_TOL)
+        sleep(0.003)
+        rate.sleep()
+        t = rate.now()
+        assert t == pytest.approx(0.02, rel=TEST_RATE_TOL)
