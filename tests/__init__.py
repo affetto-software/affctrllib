e9866238319f2f7b013fa1231a7d2882f84d1dbb
Hiroshi Atsuta <atsuta@ieee.org>
2024-04-02 17:57:39 +0900

    chore: Add __init__.py in tests directory

---
+++ tests/__init__.py
@@ -0,0 +1,12 @@
+"""Tests suite for `affctrllib`"""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+TESTS_DIR = Path(__file__).parent
+TESTS_DATA_DIR = TESTS_DIR / "data"
+CONFIG_DIR = TESTS_DATA_DIR / "config"
+
+SAMPLE_CONFIG_FILE = CONFIG_DIR / "sample_config.toml"
+ALTERNATIVE_CONFIG_FILE = CONFIG_DIR / "alternative_config.toml"
