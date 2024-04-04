74a22856cbc3b7c23e5fafb67c14741b4deb9b34
Hiroshi Atsuta <atsuta@ieee.org>
2024-04-04 17:40:56 +0900

    feat: Add AffettoMock class

---
+++ src/affctrllib/mock.py
@@ -0,0 +1,7 @@
+"""This module provides mocked Affetto instance."""
+
+from __future__ import annotations
+
+
+class AffettoMock(object):
+    pass
---
+++ tests/test_mock.py
@@ -0,0 +1,8 @@
+"""Tests for `mock` module."""
+
+from __future__ import annotations
+
+
+class TestAffettoMock:
+    def test_init(self) -> None:
+        pass
