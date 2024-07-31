ca18646df58f935e4541647080f08f1702257b64
Hiroshi Atsuta <atsuta@ieee.org>
2024-07-31 20:31:43 +0900

    chore: Use Rye for project management
    Switch a project management tool from PDM to Rye. Simplify .gitignore
    and add .python-version, requirements.lock and requirements-dev.lock
    as Rye-specific files.

--- noxfile.py
+++ noxfile.py
@@ -1,11 +1,42 @@
 import os
-
 import nox
+from pathlib import Path
 
-os.environ.update(PDM_IGNORE_SAVED_PYTHON="1", PDM_USE_VENV="1")
+nox.needs_version = ">=2024.4.15"
+nox.options.default_venv_backend = "uv|virtualenv"
+# nox.options.sessions = ["fetch", "lint", "tests"]
+nox.options.sessions = ["fetch", "tests"]
+
+PYTHON_VERSIONS = ["3.10", "3.11", "3.12"]
 
 
-@nox.session(python=("3.10", "3.11", "3.12"))
-def tests(session):
-    session.run_always("pdm", "install", "-G", "tests", external=True)
-    session.run("pytest", "tests/")
+# https://gist.github.com/MtkN1/41f36491dbf7162043d89d73a9d87dec
+def ensurepath() -> None:
+    rye_home = os.getenv("RYE_HOME")
+    rye_py = Path(rye_home) if rye_home else Path.home() / ".rye" / "py"
+
+    for py_dir in rye_py.iterdir():
+        bin_dir = py_dir / "bin"
+        os.environ["PATH"] = f"{bin_dir}:{os.environ['PATH']}"
+
+
+@nox.session()
+def fetch(session: nox.Session) -> None:
+    for require_version in PYTHON_VERSIONS:
+        session.run("rye", "fetch", require_version, external=True)
+    ensurepath()
+
+
+@nox.session(reuse_venv=True)
+def lint(session: nox.Session) -> None:
+    session.install("-r", "requirements-dev.lock")
+    session.run("ruff", "check", *session.posargs)
+
+
+@nox.session(
+    python=PYTHON_VERSIONS,
+    reuse_venv=True,
+)
+def tests(session: nox.Session) -> None:
+    session.install("-r", "requirements-dev.lock")
+    session.run("pytest", *session.posargs)
